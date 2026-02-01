0b8bf020a3c2fd0b254fa49e988c71fcf672e655
EliasOndrík <elias.ondrik@gmail.com>
2026-02-01 20:40:10 +0100

    Pridaný spôsob ukladania modelov.

--- REVSharp/ModelLoader/ModelManager.cs
+++ REVSharp/ModelLoader/ModelManager.cs
@@ -3,22 +3,44 @@ using Silk.NET.Assimp;
 
 namespace REVSharp.ModelLoader
 {
-    internal class ModelManager
+    public class ModelManager
     {
-        private Dictionary<string, Model> _loadedModels;
+        private List<Model> _loadedModels;
+        private Dictionary<string, int> _modelIndex;
         private GL _gl;
-        private Assimp _assimp;
         
         public ModelManager(GL openGl)
         {
+            _modelIndex = [];
             _loadedModels = [];
             _gl = openGl;
-            _assimp = Assimp.GetApi();
         }
         public void LoadModel(string path)
         {
-
+            if(_modelIndex.ContainsKey(path))
+            {
+                return;
+            }
+            _loadedModels.Add(new Model(_gl, path));
+            _modelIndex.Add(path, _loadedModels.Count - 1);
         }
 
+        public int GetModelIndex(string path)
+        {
+            if (_modelIndex.ContainsKey(path))
+            {
+                return _modelIndex[path];
+            }
+            return -1;
+        }
+
+        public void DrawModel(int index)
+        {
+            if (index < 0 || index >= _loadedModels.Count)
+            {
+                return;
+            }
+            _loadedModels[index].Draw();
+        }
     }
 }
