1c34ae1d523fa40ae1ee135f8df40ac3d76bf849
EliasOndrík <elias.ondrik@gmail.com>
2026-02-14 16:32:52 +0100

    Nový kamerový komponent.

---
+++ REVSharp/Components/Camera.cs
@@ -0,0 +1,15 @@
+﻿using REVSharp.Core;
+using Silk.NET.Maths;
+namespace REVSharp.Components
+{
+    public struct Camera : IComponent
+    {
+        public Vector3D<float> Target { set; get; }
+        public Vector3D<float> Up { set; get; }
+        public float FieldOfView { set; get; }
+        public float AspectRatio { set; get; }
+        public float NearPlane { set; get; }
+        public float FarPlane { set; get; }
+        public float Distance { set; get; }
+    }
+}
