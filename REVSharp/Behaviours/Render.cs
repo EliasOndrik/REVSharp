7da12a98711eafb3d5197e797d08caebe152fd0d
EliasOndrík <elias.ondrik@gmail.com>
2026-02-21 16:24:13 +0100

    Presunutie renderovania do render metody.

--- REVSharp/Behaviours/Render.cs
+++ REVSharp/Behaviours/Render.cs
@@ -22,33 +22,42 @@ namespace REVSharp.Behaviours
             _currentShaderIndex = _shaders.GetShaderIndex("default");
             _currentShader = _shaders.GetShader(_currentShaderIndex);
         }
-        public override void Update(double deltaTime, ECS componenetManager)
+        public override void OnUpdate(double deltaTime, ECS componentManager)
         {
-            uint cameraMask = componenetManager.GetComponentMask<Camera>() | componenetManager.GetComponentMask<Transform>();
+            
+        }
+        
+        public override void OnRender(double deltaTime, ECS componentManager)
+        {
+            uint cameraMask = componentManager.GetComponentMask<Camera>() | componentManager.GetComponentMask<Transform>();
             if ((_camera.ComponentMask & cameraMask) != cameraMask)
             {
                 throw new Exception("Camera entity does not have proper components. It needs Transform and Camera.");
             }
-            ref Camera cameraInfo = ref componenetManager.GetComponent<Camera>(in _camera);
-            ref Transform cameraPosition = ref componenetManager.GetComponent<Transform>(in _camera);
+            ref Camera cameraInfo = ref componentManager.GetComponent<Camera>(in _camera);
+            ref Transform cameraPosition = ref componentManager.GetComponent<Transform>(in _camera);
             Matrix4X4<float> view = Matrix4X4.CreateLookAt(cameraPosition.Position, cameraInfo.Target, cameraInfo.Up);
             Matrix4X4<float> projection = Matrix4X4.CreatePerspectiveFieldOfView(Scalar.DegreesToRadians(cameraInfo.FieldOfView), cameraInfo.AspectRatio, cameraInfo.NearPlane, cameraInfo.FarPlane);
 
             foreach (var entity in Entities)
             {
-                if (componenetManager == null)
+                if (componentManager == null)
                 {
                     continue;
                 }
-                ref Transform transform = ref componenetManager.GetComponent<Transform>(in entity);
-                ref ModelId mesh = ref componenetManager.GetComponent<ModelId>(in entity);
+                ref Transform transform = ref componentManager.GetComponent<Transform>(in entity);
+                ref ModelId mesh = ref componentManager.GetComponent<ModelId>(in entity);
+                if (!mesh.IsVisible)
+                {
+                    continue;
+                }
                 if (mesh.ShaderIndex != _currentShaderIndex)
                 {
                     _currentShaderIndex = mesh.ShaderIndex;
                     _currentShader = _shaders.GetShader(_currentShaderIndex);
                 }
                 _currentShader.Use();
-                
+
                 Matrix4X4<float> model = Matrix4X4.CreateScale(transform.Scale) *
                                         Matrix4X4.CreateRotationX(transform.Rotation.X) *
                                         Matrix4X4.CreateRotationY(transform.Rotation.Y) *
@@ -59,7 +68,7 @@ namespace REVSharp.Behaviours
                 _currentShader.SetMatrix4x4("model", model);
                 Matrix4X4.Invert(view * model, out Matrix4X4<float> result);
                 _currentShader.SetMatrix4x4("inverseModel", Matrix4X4.Transpose(result));
-                _currentShader.SetVector3D("objectColor", new(1f,1f,1f));
+                _currentShader.SetVector3D("objectColor", mesh.Color);
                 _currentShader.SetVector3D("lightColor", Vector3D<float>.One);
                 _currentShader.SetVector3D("lightPos", cameraPosition.Position);
                 _modelManager.DrawModel(mesh.ModelIndex, _currentShader);
@@ -74,5 +83,6 @@ namespace REVSharp.Behaviours
         {
             return _camera;
         }
+
     }
 }
