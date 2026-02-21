ea48a3c4a41794cc2ac52dbed3dffefb92aaac1d
EliasOndrík <elias.ondrik@gmail.com>
2026-02-21 16:22:58 +0100

    Nový nie tak celkom component

---
+++ REVSharp/Components/SimpleColisionBox.cs
@@ -0,0 +1,11 @@
+﻿using REVSharp.Core;
+using Silk.NET.Maths;
+namespace REVSharp.Components
+{
+    public struct SimpleColisionBox : IComponent
+    {
+        public Vector2D<float> OffsetX { set; get; }
+        public Vector2D<float> OffsetY { set; get; }
+        public Vector2D<float> OffsetZ { set; get; }
+    }
+}
